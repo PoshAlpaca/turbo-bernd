1fcb7943155a5dfb88d595eabceeac25c082c178
Nikolai Guyot <nikolai.guyot@code.berlin>
2020-04-11 17:25:59 +0200

    Move http example

--- src/lib.rs
+++ src/lib.rs
@@ -95,25 +95,6 @@ fn create_response(
     response
 }
 
-// "http://www.example.com/hello.txt":
-
-//      GET /hello.txt HTTP/1.1
-//      User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3
-//      Host: www.example.com
-//      Accept-Language: en, mi
-
-//      HTTP/1.1 200 OK
-//      Date: Mon, 27 Jul 2009 12:28:53 GMT
-//      Server: Apache
-//      Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT
-//      ETag: "34aa387-d-1568eb00"
-//      Accept-Ranges: bytes
-//      Content-Length: 51
-//      Vary: Accept-Encoding
-//      Content-Type: text/plain
-
-//      Hello World! My payload includes a trailing CRLF.
-
 fn handle_client(mut stream: &TcpStream) -> io::Result<String> {
     let mut buffer = [0; 1024];
     let _ = stream.read(&mut buffer[..])?;
