d30e85315035e1baaa4e8c3d8b2d56b8ead0bfe4
Nikolai Guyot <nikolai.guyot@code.berlin>
2020-04-15 13:32:03 +0200

    Move run() into new application struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use pretty_env_logger;
+use turbo_bernd::{http, routing::Router, Application};
 
 fn main() {
     pretty_env_logger::init_timed();
 
-    turbo_bernd::run();
+    let router = setup_router();
+
+    Application::new(router).run();
+}
+
+fn setup_router() -> Router {
+    let mut router = Router::new();
+
+    router.register("/hello", http::Method::Get, |_| http::Response {
+        version: http::Version::OneDotOne,
+        status: http::Status::Ok,
+        headers: http::Headers {
+            headers: Vec::new(),
+        },
+        body: "Hello, world!".to_string(),
+    });
+
+    router
 }
